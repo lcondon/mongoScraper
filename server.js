088123019594172044753586c0527e301f6bd2f4
Lucas Condon <lcondon@bennington.edu>
2018-09-11 14:59:27 -0400

    mlab

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("./public"));
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = `mongodb://heroku_7qn0s5lc:gSvvQg3YAa9d2y8@ds251632.mlab.com:51632/heroku_7qn0s5lc`;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/mongoScraper";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
